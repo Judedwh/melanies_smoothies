2dd71e7a20083bbd82d8dbac1b789355a29e8f42
Jude Wasdell Harris <132580161+Judedwh@users.noreply.github.com>
2025-05-22 13:40:55 +0100

    Update streamlit_app.py
    correctly imported streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+import streamlit as st
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(f"Customise Your Smoothie! :cup_with_straw:")
